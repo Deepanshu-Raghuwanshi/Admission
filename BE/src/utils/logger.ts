4b9e9d675bdb0fc4acc58d3840beb43ebd0755ed
Deepanshu-Raghuwanshi <dipanshuraghuwanshio@gmail.com>
2025-05-04 15:16:18 +0530

    changes for not creating file for logger

--- BE/src/utils/logger.ts
+++ BE/src/utils/logger.ts
@@ -22,15 +22,6 @@ const logger = winston.createLogger({
     new winston.transports.Console({
       format: winston.format.combine(winston.format.colorize(), logFormat),
     }),
-    ...(config.nodeEnv === "production"
-      ? [
-          new winston.transports.File({
-            filename: "logs/error.log",
-            level: "error",
-          }),
-          new winston.transports.File({ filename: "logs/combined.log" }),
-        ]
-      : []),
   ],
 });
 
